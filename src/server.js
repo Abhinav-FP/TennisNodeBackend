59b1148b2b260b6d7d891a759a44a815b07a3217
Abhinav-FP <a.mathur@futureprofilez.com>
2024-06-13 16:27:05 +0530

    vercel

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
-const { default: axios } = require("axios");
-const app = require("./app");
-const logger = require("./utils/logger");
+const express = require('express');
+const axios = require('axios');
+const logger = require('./utils/logger');
 
+const app = express();
 const port = process.env.PORT || 5000;
 
-// const checkApiAuth = async () => {
+app.use(express.json()); // Middleware to parse JSON
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// app.post('/auth-verify', async (req, res) => {
 //   const Api = axios.create({
 //       baseURL: 'https://dev-control.tenniskhelo.com/api',
 //       headers:{
@@ -12,18 +19,16 @@ const port = process.env.PORT || 5000;
 //       }
 //   });
 
-//   await Api.post('auth-verify', {username: 'pradeep@fpdemo.com', password:'Pradeep#Fp-Dev09'})
-//       .then(resp => {
-//           console.log('resp-data', resp.data);
-//       }).catch(_err => {
-//           console.log('auth-error', _err);
-//       });
-// }
-// checkApiAuth();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+//   try {
+//     const response = await Api.post('auth-verify', {
+//         username: 'pradeep@fpdemo.com',
+//         password: 'Pradeep#Fp-Dev09'
+//     });
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Authentication failed' });
+//   }
+// });
 
 app.listen(port, () => {
   logger.info(`Server listening at http://localhost:${port}`);
