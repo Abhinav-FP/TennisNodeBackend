79413f1f77459c28d6df276eaa949eebb959f0b5
Abhinav-FP <a.mathur@futureprofilez.com>
2024-06-21 17:02:19 +0530

    Netlify functions

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const app = require("./src/app.js");
-// const logger = require("./utils/logger");
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  // logger.info(`Server listening at http://localhost:${port}`);
   console.log(`Server listening at http://localhost:${port}`);
 });
--- src/app.js
+++ src/app.js
@@ -2,34 +2,31 @@ const express = require("express");
 const serverless = require("serverless-http");
 const pdfRoutes = require("./routes/pdfRoutes");
 const { errorHandler } = require("./utils/errorHandler");
+const cors = require("cors");
+
+const app = express();
 const router = express.Router();
 
-const cors = require("cors");
 const corsOptions = {
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, 
+  credentials: true,
 }; 
 
-const app = express();
-app.get("/", (req, res) => {
-  res.json({
-    msg:'hello ',
-    status:200
-  })
-});
-// Apply CORS middleware
 app.use(cors(corsOptions)); 
-
 app.use(express.json({ limit: '50mb' }));
 
+router.get("/", (req, res) => {
+  res.json({
+    msg: 'hello',
+    status: 200,
+  });
+});
+
+router.use("/api/extract", pdfRoutes);
+
 app.use("/.netlify/functions/app", router);
-module.exports.handler = serverless(app);
-// const pdfRoutes = require("./routes/pdfRoutes");
-
-app.use("/api/extract", pdfRoutes);
-
-
 app.use(errorHandler);
 
 module.exports = app;
+module.exports.handler = serverless(app);
