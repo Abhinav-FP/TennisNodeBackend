fd0f8f2419f40ba87f8ca59845455013b5364d3f
Abhinav-FP <a.mathur@futureprofilez.com>
2024-06-26 14:45:23 +0530

    pdf-js dist

--- app.js
+++ app.js
@@ -7,15 +7,12 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
 const router = express.Router();
-const pdfjsLib = require('pdfjs-dist');
 
 const corsOptions = {
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }; 
-
-
 
 app.use(cors(corsOptions)); 
 app.use(express.json({ limit: '50mb' }));
@@ -26,8 +23,6 @@ app.get("/", (req, res) => {
     status: 200,
   });
 });
-// Specify the URL to the worker
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.worker.min.js';
 
 app.use("/api/extract", pdfRoutes);
 
@@ -37,5 +32,18 @@ app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
 module.exports = app;
 module.exports.handler = serverless(app);
+
+// Dynamically import pdfjs-dist and set workerSrc
+import('pdfjs-dist').then(pdfjsLib => {
+  // Ensure pdfjsLib is correctly loaded before setting workerSrc
+  if (pdfjsLib.GlobalWorkerOptions) {
+    pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.worker.min.js';
+  } else {
+    console.error('pdfjsLib.GlobalWorkerOptions is not available.');
+  }
+}).catch(err => {
+  console.error('Error importing pdfjs-dist:', err);
+});
